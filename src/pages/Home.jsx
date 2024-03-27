8464da012de441a9014b50c5bdc12a54870877e9
Raghav Sabharwal <ragsabharwal369@gmail.com>
2024-03-27 16:43:56 +0530

    done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div id="home-page">
+      <h1>Home page</h1>
+      <h2>Hi, my name is Siddharth</h2>
+      <h2>
+        and I work at <a href="https://www.newtonschool.co/">Newton School</a>
+      </h2>
+
+      <button id="home-to-blog">Go to Blog</button>
+      <button id="home-to-info">Go to Info</button>
+    </div>
+  );
+};
+export default Home;
